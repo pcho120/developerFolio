444b491c04520b587ea7406a9cbf75872348a401
paul120 <79517345+pcho120@users.noreply.github.com>
2026-07-05 23:16:23 -0400

    Update Contact.js

--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -1,8 +1,9 @@
 import React, {useContext} from "react";
 import "./Contact.scss";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
-import {illustration, contactInfo, resumeSection, greeting} from "../../portfolio"; // 👈 여기에 greeting을 추가했습니다.
+import {illustration, contactInfo} from "../../portfolio";
 import {Fade} from "react-reveal";
+import email from "../../assets/lottie/email";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import StyleContext from "../../contexts/StyleContext";
 
@@ -10,19 +11,30 @@ export default function Contact() {
   const {isDark} = useContext(StyleContext);
   return (
     <Fade bottom duration={1000} distance="20px">
-      <div className="main" id="contact">
+      <div className="main contact-margin-top" id="contact">
         <div className="contact-div-main">
           <div className="contact-header">
-            <h1 className={isDark ? "dark-mode heading" : "heading"}>
-              {contactInfo.title}
-            </h1>
-            <p className={isDark ? "dark-mode subTitle" : "subTitle"}>
+            <h1 className="heading contact-title">{contactInfo.title}</h1>
+            <p
+              className={
+                isDark
+                  ? "dark-mode contact-subtitle"
+                  : "subTitle contact-subtitle"
+              }
+            >
               {contactInfo.subtitle}
             </p>
-            <div className={isDark ? "dark-mode contact-text-div" : "contact-text-div"}>
+            <div
+              className={
+                isDark ? "dark-mode contact-text-div" : "contact-text-div"
+              }
+            >
               {contactInfo.number && (
                 <>
-                  <a className="contact-detail" href={"tel:" + contactInfo.number}>
+                  <a
+                    className="contact-detail"
+                    href={"tel:" + contactInfo.number}
+                  >
                     {contactInfo.number}
                   </a>
                   <br />
@@ -42,42 +54,15 @@ export default function Contact() {
           </div>
           <div className="contact-image-div">
             {illustration.animated ? (
-              <DisplayLottie animationData={require("../../assets/lottie/contactMail.json")} />
+              <DisplayLottie animationData={email} />
             ) : (
               <img
                 alt="Man working"
-                src={require("../../assets/images/contactMail.svg")}
-              />
+                src={require("../../assets/images/contactMailDark.svg")}
+              ></img>
             )}
           </div>
         </div>
-        
-        {/* 👇 Email banner at the bottom of the page */}
-        {resumeSection.display && (
-          <div className="resume-main-div">
-            <div className="resume-header">
-              <h1 className={isDark ? "dark-mode heading" : "heading"}>
-                {resumeSection.title}
-              </h1>
-              <p className={isDark ? "dark-mode subTitle" : "subTitle"}>
-                {resumeSection.subtitle}
-              </p>
-            </div>
-            <div className="resume-content-div">
-              {/* it will connect google drive and open in new tab */}
-              <a
-                className="btn-text"
-                href={greeting.resumeLink}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <button className="main-button">
-                  View My Resume
-                </button>
-              </a>
-            </div>
-          </div>
-        )}
       </div>
     </Fade>
   );
